f9fcb4173a9f6b03960e1c84924b984ab091df55
Axel Giottonini <axel.giottonini@unifr.ch>
2023-07-20 16:32:02 +0200

    cli implementation

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,4 @@ from .dataset import Dataset
 from .tokenizer import Tokens, MaskedTokens
 from .utils import train_loop, loss_fn
 from .getters import get_model, get_dataloaders
+from .cli import configure, summary
